2424bb569afca10db2624867904b7db767119643
Brian Sohn <sohnbrian.ss.ss@gmail.com>
2022-12-07 18:30:35 -0500

    added generating 5 character passwords

--- tests.py
+++ tests.py
@@ -2,9 +2,27 @@
 
 import hashlib
 import csv
+import random
+import string 
 
-PLAINTXTS = ['aaaaa','bbbbb','AAAAA','aAaAa','zzzZZ','abcde']
+
+#PLAINTXTS = ['aaaaa','bbbbb','AAAAA','aAaAa','zzzZZ','abcde']
+PLAINTXTS = []
 FIELDS = ['plaintext', 'hashed_val']
+
+def gen_single_random_plaintext():
+
+    rand = string.ascii_letters
+    random_plain = ''.join(random.choice(rand) for i in range(5))
+    
+    return random_plain
+
+def gen_multi_random_plaintext(size):
+
+    for i in range(size):
+        PLAINTXTS.append(gen_single_random_plaintext())
+    
+    #print(PLAINTXTS)
 
 def gen_hash_pairs():
 
@@ -20,6 +38,9 @@ def gen_hash_pairs():
     return hashes
 
 def main():
+
+    gen_multi_random_plaintext(20)
+
     hash_pairs = gen_hash_pairs()
     print('Hash pairs:', hash_pairs)
 
@@ -40,3 +61,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
