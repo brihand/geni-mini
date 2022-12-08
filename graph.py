20a52d674b4b2b7e0daf442316d110f4a1969197
Brian Sohn <sohnbrian.ss.ss@gmail.com>
2022-12-08 15:22:54 -0500

    graph added

---
+++ graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x_axis = ['value_1', 'value_2', 'value_3', ...]
+y_axis = ['value_1', 'value_2', 'value_3', ...]
+
+plt.plot(x_axis, y_axis)
+plt.title('title name')
+plt.xlabel('x_axis name')
+plt.ylabel('y_axis name')
+plt.show()
